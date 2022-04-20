5ffab9a40697820e1cc8276db94721d096159901
Fabio Nolasco <fabio@phonexinc.com>
2022-04-20 16:13:05 -0400

    My first commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+var AWS = require("aws-sdk");
+const path = require('path')
+const app = express()
+const port = 3000
+
+AWS.config.getCredentials(function(err) {
+    if (err) console.log(err.stack);
+    // credentials not loaded
+    else {
+      console.log("Access key:", AWS.config.credentials.accessKeyId);
+    }
+  });
+
+const bible = {
+    books: [
+        { title: "Genesis" },
+        { title: "Exodus" },
+        { title: "Leviticus" },
+        { title: "Numbers" },
+        { title: "Deuteronomy" },
+    ]
+};
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', `index.html`))
+})
+
+app.get('/bible', (req, res) => {
+    res.json(JSON.stringify(bible))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
